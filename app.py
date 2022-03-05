dd245adad0d2a33608dd5979c1019a0de076ea78
asitnayak <asitworld123@gmail.com>
2022-03-05 20:23:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template
+from model import Model
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+
+file_handler = logging.FileHandler("Logs/main.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def prediction():
+    try:
+        print("Hi")
+
+        logger.debug("Starting Prediction process.")
+
+        input_data = [i for i in request.form.values()]
+        logger.debug("Captured input data.")
+        obj = Model(input_data)
+        logger.debug("Model object created.")
+        obj.generate_dataframe()
+        logger.debug("Input DataFrame generated.")
+        obj.preprocessor()
+        logger.debug("Data preprocessed.")
+        y_hat = obj.soft_voting_prediction()
+        logger.debug("Received prediction.")
+        #print(y_hat)
+        result = "Try Again"
+
+        if y_hat == 1:
+            result = "Income is more than $50K"
+        elif y_hat == 0:
+            result = "Income is less than $50K"
+
+        return render_template('index.html', result=result)
+
+    except:
+        logger.debug("Error occurred while predicting.")
+        return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,103 @@
+from xgboost import XGBClassifier
+import logging
+import pickle
+import pandas as pd
+import numpy as np
+import warnings
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+
+file_handler = logging.FileHandler("Logs/model.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+warnings.filterwarnings('ignore')
+
+
+class Model:
+    def __init__(self, input_data):
+        logger.debug("Created Model object.")
+
+        self.input_data = input_data
+        self.columns = ['age', 'workclass', 'fnlwgt', 'education', 'marital-status','occupation', 'relationship', 'race', 'sex', 'capital-gain','capital-loss', 'hours-per-week', 'country']
+        self.cat = ['workclass', 'education', 'marital-status', 'occupation','relationship', 'race', 'sex', 'country']
+        self.num = ['capital-gain', 'capital-loss', 'age', 'fnlwgt', 'hours-per-week']
+        self.df = None
+        self.scalar = pickle.load(open("pick/scalar.sav", "rb"))
+        self.oe = pickle.load(open("pick/oe.sav", "rb"))
+        self.dt = pickle.load(open("pick/tuned_dt_2.sav", "rb"))
+        self.knn = pickle.load(open("pick/tuned_knn_2.sav", "rb"))
+        self.rf = pickle.load(open("pick/tuned_rf_2.sav", "rb"))
+        self.xgb = XGBClassifier()
+        self.xgb.load_model("pick/tuned_xgb_4.bin")
+
+    def generate_dataframe(self):
+        try:
+            logger.debug("Inside generate_dataframe. Generating DataFrame out of the input data.")
+
+            input_df = pd.DataFrame(columns=self.columns)
+            input_df.loc[0] = self.input_data
+            self.df = input_df
+
+            logger.debug("Exiting generate_dataframe successfully !!!")
+
+        except Exception as e:
+            logger.debug("Error occurred inside generate_dataframe of Model class.")
+            raise e
+
+    def preprocessor(self):
+        try:
+            logger.debug("Inside preprocessor. Preprocessing the input dataframe.")
+
+            self.df.loc[:, self.num] = self.scalar.transform(self.df[self.num])
+            self.df.loc[:, self.cat] = self.oe.transform(self.df[self.cat])
+            for i in self.num:
+                self.df[i] = np.array(self.df[i], dtype='float64')
+
+            logger.debug("Exiting preprocessor successfully !!!")
+
+        except Exception as e:
+            logger.debug("Error occurred inside preprocessor of Model class.")
+            raise e
+
+    def soft_voting_prediction(self):
+        try:
+            logger.debug("Inside soft_voting_prediction. Starting prediction.")
+
+            zeroes = []
+            ones = []
+
+            temp_predict = self.xgb.predict_proba(self.df.to_numpy())
+            zeroes.append(temp_predict[0][0])
+            ones.append(temp_predict[0][1])
+
+            temp_predict = self.rf.predict_proba(self.df.to_numpy())
+            zeroes.append(temp_predict[0][0])
+            ones.append(temp_predict[0][1])
+
+            temp_predict = self.knn.predict_proba(self.df.to_numpy())
+            zeroes.append(temp_predict[0][0])
+            ones.append(temp_predict[0][1])
+
+            temp_predict = self.dt.predict_proba(self.df.to_numpy())
+            zeroes.append(temp_predict[0][0])
+            ones.append(temp_predict[0][1])
+
+            zeroes = np.array(zeroes)
+            ones = np.array(ones)
+
+            y_proba = [zeroes.mean(), ones.mean()]
+            #print(y_proba)
+            y_hat = y_proba.index(max(y_proba))
+
+            logger.debug("Exiting soft_voting_prediction successfully !!!")
+
+            return y_hat
+
+        except Exception as e:
+            logger.debug("Error occurred inside soft_voting_prediction of Model class.")
+            raise e
